143f1216523674bb2ab9750082ba8cbb5d8304be
Porcelain-vip <Porcelain.vip@qq.com>
2017-03-23 22:23:11 +0800

    优化代码

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -5,18 +5,19 @@ void MergeSortHelp(Type* a, Type* b, int left, int right)
 {
 	if (left < right)
 	{
-		int mid = (left + right) / 2;
+		int mid = left + (right - left) / 2;                //also    int mid = (left + right) / 2;
 		MergeSortHelp<Type>(a, b, left, mid);
 		MergeSortHelp<Type>(a, b, mid + 1, right);
 
 		int i, low, high;
-		for (i = left, low = left, high = mid + 1; low <= mid && high <= right; ++i)
+		for (i = left, low = left, high = mid + 1; low <= mid && high <= right;)
 		{
 			if (a[low] <= a[high]) { b[i] = a[low]; ++low; }
 			else { b[i] = a[high]; ++high; }
+			++i;
 		}
-		for (; low <= mid; ++low, ++i) b[i] = a[low];
-		for (; high <= right; ++high, ++i) b[i] = a[high];
+		for (; low <= mid;) { b[i] = a[low];  ++low; ++i; }
+		for (; high <= right;) { b[i] = a[high]; ++high; ++i; }
 		for (i = left; i <= right; ++i) a[i] = b[i];
 	}
 }
@@ -31,7 +32,7 @@ void MergeSort(Type* a, int n)
 
 int main()
 {
-	using std::cin; using std::cout; using std::endl;
+	using namespace std;
 	int a[10];
 	cout << "Please enter 10 numbers as you like: ";
 	for (int& value : a) cin >> value;
